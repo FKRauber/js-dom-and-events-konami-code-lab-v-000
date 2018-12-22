a0ea0f72a6f0dfc90018464a7400553653b483e0
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-22 03:49:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let i = 0;
 
-  function
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+  })
 }
